5c223b3ba817f54eeb442bfc7cc3061da78ab043
Alexander Chekel <alexandr.chekel@gmail.com>
2024-04-12 16:23:00 +0300

    Added touch drawing logic

--- Sources/TouchIndicators/UIWindow+TouchEvents.swift
+++ Sources/TouchIndicators/UIWindow+TouchEvents.swift
@@ -16,8 +16,8 @@ extension UIWindow {
         }
     }
 
-    /// Indicates whether method swizzling has already occurred.
     private static var didSwizzleMethods: Bool = false
+    private static var touchIndicatorLayers: [UITouch: CALayer] = [:]
 
     private static func swizzleMethodsIfNeeded() {
         guard !didSwizzleMethods else {
@@ -39,8 +39,37 @@ extension UIWindow {
         didSwizzleMethods = true
     }
 
-    /// Custom implementation of sendEvent(_:) instance method.
     @objc private dynamic func swizzled_sendEvent(_ event: UIEvent) {
         swizzled_sendEvent(event)
+
+        guard Self.showsTouches, case .touches = event.type, let touches = event.allTouches else {
+            return
+        }
+
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+
+            switch touch.phase {
+                case .began:
+                    let touchIndicatorLayer = CALayer()
+                    touchIndicatorLayer.backgroundColor = UIColor.gray.withAlphaComponent(0.5).cgColor
+                    touchIndicatorLayer.borderWidth = 1
+                    touchIndicatorLayer.borderColor = UIColor.gray.cgColor
+                    touchIndicatorLayer.cornerRadius = 15
+                    touchIndicatorLayer.frame = CGRect(x: touchLocation.x - 15, y: touchLocation.y - 15, width: 30, height: 30)
+                    layer.addSublayer(touchIndicatorLayer)
+                    Self.touchIndicatorLayers[touch] = touchIndicatorLayer
+                case .moved:
+                    CATransaction.begin()
+                    CATransaction.setDisableActions(true)
+                    Self.touchIndicatorLayers[touch]?.position = touchLocation
+                    CATransaction.commit()
+                case .ended, .cancelled:
+                    Self.touchIndicatorLayers[touch]?.removeFromSuperlayer()
+                    Self.touchIndicatorLayers[touch] = nil
+                default:
+                    break
+            }
+        }
     }
 }
